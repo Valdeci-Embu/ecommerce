4a4d4fa0489e34dddbb18b5087513704e38f1a25
Valdeci <valdecie@gmail.com>
2024-02-07 19:16:02 -0300

    adicinado produto

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,24 @@
+"""Module that provides a python version of package import"""
 from django.contrib import admin
+from .models import Produto, Variacao
+from .forms import VariacaoObrigatoria
 
-# Register your models here.
+
+class VariacaoInline(admin.TabularInline):
+    model = Variacao
+    formset = VariacaoObrigatoria
+    min_num = 1
+    extra = 0
+    can_delete = True
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao_curta',
+                    'get_preco_formatado', 'get_preco_promocional_formatado']
+    inlines = [
+        VariacaoInline
+    ]
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Variacao)
